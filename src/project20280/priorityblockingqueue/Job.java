e9e52856ae2a83bb48a7d0f564550bd8d70d9516
SofMovUCD <Sofia.movshovich@ucdconnect.ie>
2026-03-26 18:04:19 +0000

    made Job implement Comparable

--- src/project20280/priorityblockingqueue/Job.java
+++ src/project20280/priorityblockingqueue/Job.java
@@ -2,21 +2,51 @@ package project20280.priorityblockingqueue;
 
 import javax.print.attribute.standard.JobPriority;
 
-public class Job implements Runnable{
+import static java.util.Objects.compare;
+
+public class Job implements Runnable, Comparable<Job>{
     private String jobName;
     private project20280.priorityblockingqueue.JobPriority jobPriority;
+    private Integer expectedDuration;
 
     public Job(String job1, project20280.priorityblockingqueue.JobPriority jobPriority) {
         this.jobName = job1;
         this.jobPriority = jobPriority;
+        expectedDuration = null;
+    }
+
+    public Job(String job1, int eDuration,project20280.priorityblockingqueue.JobPriority jobPriority) {
+        this.jobName = job1;
+        this.jobPriority = jobPriority;
+        expectedDuration = eDuration;
+    }
+
+    @Override
+    public int compareTo(Job other){
+        //initially compare job priority
+        int priorityCompare = other.jobPriority.compareTo(this.jobPriority);
+
+        if (priorityCompare != 0) {
+            return priorityCompare;
+        }
+
+        //then compare duration
+        return Long.compare(this.expectedDuration, other.expectedDuration);
     }
 
     @Override
     public void run(){
-        System.out.println("Job: "+jobName+" Priority: "+jobPriority);
+        System.out.println("Job: "+jobName+" Priority: "+jobPriority + " Expected duration: "+ ((expectedDuration == null)? "N/A": expectedDuration));
         //do some work
+        int ts;
+        if(expectedDuration == null){
+            ts = 1000;
+        }
+        else{
+            ts = expectedDuration*100;
+        }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(ts);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -24,5 +54,9 @@ public class Job implements Runnable{
 
     public project20280.priorityblockingqueue.JobPriority getJobPriority() {
         return jobPriority;
+    }
+
+    public Integer getExpectedDuration() {
+        return expectedDuration;
     }
 }
