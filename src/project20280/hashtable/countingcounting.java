1d260250fede9b0e6793bc65025fe987a8f73e69
SofMovUCD <Sofia.movshovich@ucdconnect.ie>
2026-03-30 23:12:41 +0100

    finished Q5

--- src/project20280/hashtable/countingcounting.java
+++ src/project20280/hashtable/countingcounting.java
@@ -1,24 +1,45 @@
 package project20280.hashtable;
 
+import project20280.interfaces.Entry;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class countingcounting {
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("sample_text.txt");
+        File f = new File("C:\\Users\\mysof\\IdeaProjects\\datastructures2-SofMovUCD\\sample_text.txt");
 
         ChainHashMap<String, Integer> counter = new ChainHashMap<>();
 
         Scanner scanner = new Scanner(f);
 
         while(scanner.hasNext()){
-            String word = scanner.next();
-            System.out.println("word: "+word);
+            String word = scanner.next().toLowerCase();
+            //System.out.println("word: "+word);
 
             //if word not in hashmap add it with count 1
+            if(counter.get(word) == null){
+                counter.put( word, 1); //add number
+            }
+            else{ //if word in hashmap, increment count by 1
+                counter.put(word, counter.get(word)+1);
+            }
+        }
 
-            //if word in hashmap, increment count by 1
+        ArrayList<Entry<String, Integer>> topTen = new ArrayList<>();
+        for(Entry<String,Integer> e : counter.entrySet()){
+            topTen.add(e);
+            if(topTen.size() > 10){
+                topTen.sort((a,b) -> b.getValue().compareTo(a.getValue()));
+                topTen.removeLast();
+            }
+        }
+
+        for(Entry<String,Integer> e : topTen){
+            System.out.println("Word: "+ e.getKey()+", Frequency: "+ e.getValue());
         }
     }
 }
